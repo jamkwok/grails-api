391efffcc3444058d1e99a3b73ae20a2a0dda5e0
James Kwok <jameskwok@L-C02V81BCHV2J-James-Kwok.local>
2017-09-07 16:53:06 +1000

    init

--- api/grails-app/controllers/api/UrlMappings.groovy
+++ api/grails-app/controllers/api/UrlMappings.groovy
@@ -12,5 +12,8 @@ class UrlMappings {
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
+        //
+        // RESTService api
+        "/api/city"(resources: 'city')
     }
 }
